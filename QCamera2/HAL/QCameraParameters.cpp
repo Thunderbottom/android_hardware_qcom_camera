963678435830e4e92462ada02e43a888f7c5de10
Sultanxda <sultanxda@gmail.com>
2015-07-13 13:12:27 -0700

    QCamera2: Disable JPEG compression

--- QCamera2/HAL/QCameraParameters.cpp
+++ QCamera2/HAL/QCameraParameters.cpp
@@ -7688,11 +7688,7 @@ int QCameraParameters::getBurstNum()
  *==========================================================================*/
 int QCameraParameters::getJpegQuality()
 {
-    int quality = getInt(KEY_JPEG_QUALITY);
-    if (quality < 0) {
-        quality = 98; // set to default quality value
-    }
-    return quality;
+    return 100;
 }
 
 
