ac3bbc1706e31329c6a3e5d1428b731351bdd9ca
Sultanxda <sultanxda@gmail.com>
2016-03-05 01:55:29 -0800

    QCamera2: Enable support for manual ISO and exposure time controls
    *Set max-supported exposure time to 16 seconds
    *Enabled coexistence of manual ISO/exposure time controls with anti-shake
     algorithm

--- QCamera2/HAL/QCameraParameters.cpp
+++ QCamera2/HAL/QCameraParameters.cpp
@@ -608,6 +608,8 @@ const QCameraParameters::QCameraMap QCameraParameters::CDS_MODES_MAP[] = {
 #define TOTAL_RAM_SIZE_512MB 536870912
 
 static uint32_t mPrvwExpTimeUs = 0;
+static bool mIsManualIso = false;
+static bool mIsManualExpTime = false;
 
 /*===========================================================================
  * FUNCTION   : QCameraParameters
@@ -2602,7 +2604,8 @@ int32_t  QCameraParameters::setExposureTime(const QCameraParameters& params)
     const char *prev_str = get(KEY_QC_EXPOSURE_TIME);
     if (str != NULL) {
         if (prev_str == NULL ||
-            strcmp(str, prev_str) != 0) {
+            strcmp(str, prev_str) != 0 ||
+            (strcmp(str, "0") && mPrvwExpTimeUs)) {
             return setExposureTime(str);
         }
     }
@@ -3845,6 +3848,17 @@ int QCameraParameters::getPrvwExpTime()
     return mPrvwExpTimeUs;
 }
 
+bool QCameraParameters::isManualMode()
+{
+    bool manualMode;
+
+    manualMode = mIsManualIso || mIsManualExpTime;
+    if (manualMode)
+        mPrvwExpTimeUs = 0;
+
+    return manualMode;
+}
+
 /*===========================================================================
  * FUNCTION   : updateParameters
  *
@@ -4325,8 +4339,8 @@ int32_t QCameraParameters::initDefaultParameters()
     setISOValue(ISO_AUTO);
 
     // Set exposure time, we should get them from m_pCapability
-    m_pCapability->min_exposure_time = 200;
-    m_pCapability->max_exposure_time = 2000000;
+    m_pCapability->min_exposure_time = 100;
+    m_pCapability->max_exposure_time = 16000000;
     set(KEY_QC_MIN_EXPOSURE_TIME, m_pCapability->min_exposure_time);
     set(KEY_QC_MAX_EXPOSURE_TIME, m_pCapability->max_exposure_time);
     //setExposureTime("0");
@@ -5362,7 +5376,6 @@ int32_t QCameraParameters::setZoom(int zoom_level)
  *==========================================================================*/
 int32_t  QCameraParameters::setISOValue(const char *isoValue)
 {
-#if 0
     char iso[PROPERTY_VALUE_MAX];
     int32_t continous_iso = 0;
     // Check if continuous ISO is set
@@ -5379,6 +5392,14 @@ int32_t  QCameraParameters::setISOValue(const char *isoValue)
         int32_t value = lookupAttr(ISO_MODES_MAP,
                                    sizeof(ISO_MODES_MAP)/sizeof(QCameraMap),
                                    isoValue);
+
+        if (value != CAM_ISO_MODE_AUTO) {
+            mIsManualIso = true;
+            setPrvwExpTime(0);
+        } else {
+            mIsManualIso = false;
+        }
+
         if (value != NAME_NOT_FOUND) {
             CDBG("%s: Setting ISO value %s", __func__, isoValue);
             updateParamEntry(KEY_QC_ISO_MODE, isoValue);
@@ -5391,8 +5412,6 @@ int32_t  QCameraParameters::setISOValue(const char *isoValue)
     ALOGE("Invalid ISO value: %s",
           (isoValue == NULL) ? "NULL" : isoValue);
     return BAD_VALUE;
-#endif
-    return NO_ERROR;
 }
 
 /*===========================================================================
@@ -5409,7 +5428,6 @@ int32_t  QCameraParameters::setISOValue(const char *isoValue)
  *==========================================================================*/
 int32_t  QCameraParameters::setExposureTime(const char *expTimeStr)
 {
-#if 0
     if (expTimeStr != NULL) {
         int32_t expTimeUs = atoi(expTimeStr);
         int32_t min_exp_time = m_pCapability->min_exposure_time; /* 200 */
@@ -5418,10 +5436,14 @@ int32_t  QCameraParameters::setExposureTime(const char *expTimeStr)
         // Cap exposure time to upper/lower limits without returning an error
         // to prevent crashes in CameraNext
         if (expTimeUs) {
+            mIsManualExpTime = true; 
+            setPrvwExpTime(0);
             if (expTimeUs > max_exp_time)
                 expTimeUs = max_exp_time;
             else if (expTimeUs < min_exp_time)
                 expTimeUs = min_exp_time;
+        } else {
+            mIsManualExpTime = false;
         }
 
         // expTime == 0 means not to use manual exposure time.
@@ -5439,8 +5461,6 @@ int32_t  QCameraParameters::setExposureTime(const char *expTimeStr)
     ALOGE("Invalid exposure time, value: %s",
           (expTimeStr == NULL) ? "NULL" : expTimeStr);
     return BAD_VALUE;
-#endif
-    return NO_ERROR;
 }
 
 /*===========================================================================
